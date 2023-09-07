561e039aa6a5e41d5cb651887960a10b4198b8fb
SilasRSA <silasrsalves@alu.ufc.br>
2023-09-07 09:10:37 -0300

    create lab4

---
+++ lab4/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { Container, StyleSheet, Header, Content, Form, Item, Input, Text } from 'native-base';
+
+export default function app() {
+  const [name, setName] = useState('');
+  return (
+    <Container>
+      <Header />
+      <Content>
+        <Text style={{fontWeight:'bold'}}>What is your name?</Text>
+        <Form>
+          <Item last> 
+            <Input 
+              placeholder='John Bunyan'
+              onChangeText={text => setName(text)}
+            />
+          </Item>
+        </Form>
+        <Text style={{fontWeight:'bold',color:'#00ffff', alignSelf:'center'}}>Say hello {name}</Text>
+      </Content>
+    </Container>
+  );
+}
